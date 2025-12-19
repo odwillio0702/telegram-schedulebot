25b28722d5c883bf396e54301f915574bcdc5c8d
odwillio0702 <odwillio21100@gmail.com>
2025-12-19 19:50:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot
+from telebot import types
+
+BOT_TOKEN = "8485092572:AAHIdjrrXBOaIPD6-wN17cXtxleHYOWxJiw"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# ================== Команды ==================
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.InlineKeyboardMarkup()
+    # Кнопка для открытия Mini App
+    web_app = types.WebAppInfo(url="https://твояссылка-на-mini-app.vercel.app")
+    button = types.InlineKeyboardButton(text="Открыть приложение", web_app=web_app)
+    keyboard.add(button)
+    
+    bot.send_message(message.chat.id, "!?/$%??!", reply_markup=keyboard)
+
+# ================== Старт бота ==================
+bot.infinity_polling()
