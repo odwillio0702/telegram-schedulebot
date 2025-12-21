203983f298ffc0a641ab119c5c3af17b5cf06bc1
odwillio0702 <odwillio21100@gmail.com>
2025-12-21 01:12:55 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,4 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-WEBAPP_URL = os.getenv(
-    "WEBAPP_URL",
-    "https://odwillio0702.github.io/personalinfo/"
-)
+WEBAPP_URL = os.getenv("https://odwillio0702.github.io/personalinfo/")  # или твоя ссылка на GitHub Pages
